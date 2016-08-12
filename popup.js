b3498d1789202eacdac4cabe5a9c4dfc79059b36
George <gpickett00@gmail.com>
2016-08-11 18:13:26 -0700

    took out config stuff

--- popup.js
+++ popup.js
@@ -1,10 +1,4 @@
-var authToken;
-var config = {
-    apiKey: "AIzaSyBZtw9sDtZKhI5Q6w--kwulEpv6d8UPUwo",
-    authDomain: "search-feed-35574.firebaseapp.com",
-    databaseURL: "https://search-feed-35574.firebaseio.com",
-    storageBucket: "search-feed-35574.appspot.com",
-};
+
 
 firebase.initializeApp(config);
 
