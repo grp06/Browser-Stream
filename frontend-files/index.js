c0608cf623af63f2ad56bbd46236a4b1daa16cff
George <gpickett00@gmail.com>
2016-08-11 17:54:36 -0700

    changes to gitignore

--- frontend-files/index.js
+++ frontend-files/index.js
@@ -1,9 +1,4 @@
-var config = {
-    apiKey: "AIzaSyBZtw9sDtZKhI5Q6w--kwulEpv6d8UPUwo",
-    authDomain: "search-feed-35574.firebaseapp.com",
-    databaseURL: "https://search-feed-35574.firebaseio.com",
-    storageBucket: "search-feed-35574.appspot.com",
-};
+console.log('config = ', config)
 
 firebase.initializeApp(config);
 
---
+++ frontend-files/keys.js
@@ -0,0 +1,6 @@
+config = {
+    apiKey: "AIzaSyBZtw9sDtZKhI5Q6w--kwulEpv6d8UPUwo",
+    authDomain: "search-feed-35574.firebaseapp.com",
+    databaseURL: "https://search-feed-35574.firebaseio.com",
+    storageBucket: "search-feed-35574.appspot.com",
+};
